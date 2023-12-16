5294de08526bc2b8bd15a12a7492116c1f58a658
Bit Handshake <adam@monotech.hu>
2023-12-16 15:42:50 +0100

    0.0.1.3

--- source-code/clj/source_code_index/export/config.clj
+++ source-code/clj/source_code_index/export/config.clj
@@ -7,4 +7,4 @@
 ; @ignore
 ;
 ; @constant (regex pattern)
-(def VERSION-PATTERN #"(?<=\n\#\#\#\s\[).*(?=\]\s\-\s[\d]{4,4}\-[\d]{2,2}\-[\d]{2,2})")
+(def VERSION-PATTERN #"(?<=\n\#\#\# \[).*(?=\] \- [\d]{4,4}\-[\d]{2,2}\-[\d]{2,2})")
